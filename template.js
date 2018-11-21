b92b3ba024c15066ea51a4bd4489919b17b7b833
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2018-11-21 21:08:14 -0200

    Implementação de proxy para atualização reativa da lista

--- template.js
+++ template.js
@@ -1,25 +1,34 @@
 const language = document.querySelector('[data-js="language"]');
 const resultsList = document.querySelector('[data-js="results-list"]');
 const templateWorker = new Worker('template-worker.js');
-const config = {
-  languageTag: 'en-US',
-  list: []
-}
+const config = new Proxy(
+  {
+    languageTag: 'en-US',
+    list: []
+  },
+  {
+    set: function (target, prop, value) {
+      if (prop === 'languageTag' || prop === 'list') {
+        Reflect.set(...arguments);
+        render();
+        return true;
+      }
+      return false;
+    }
+  });
 
 language.addEventListener('change', handleLanguageSelection);
 
 function handleLanguageSelection() {
   config.languageTag = this.value;
-  render();
 }
 
 function setList(newList) {
   config.list = newList;
-  render();
 }
 
 function render() {
-  templateWorker.postMessage(config);
+  templateWorker.postMessage(Object.assign({}, config));
   templateWorker
     .addEventListener('message', ({ data }) => resultsList.innerHTML = data);
 }
